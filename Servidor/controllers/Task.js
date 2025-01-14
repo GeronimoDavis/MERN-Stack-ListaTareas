737b2789f2108010b2e998c5fb1761f47c755b76
GeronimoDavis <geronimodavis@gmail.com>
2025-01-14 16:30:50 -0300

    Usando react

--- Servidor/controllers/Task.js
+++ Servidor/controllers/Task.js
@@ -19,6 +19,7 @@ const createTask = async(req, res) => {
         res.status(201).json(task);
         
     }catch(error){
+        console.log(error);
         res.status(500).json({message: error.message});
     }
 }
