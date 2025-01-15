26879ddf85409869354b9d313d486a89489d7d80
GeronimoDavis <geronimodavis@gmail.com>
2025-01-14 23:32:30 -0300

    Mostrar lista de tareas

---
+++ Cliente/MERN-ListaTareas/src/componentes/TaskList.jsx
@@ -0,0 +1,37 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+const TaskList = ({onEdit, onDelete}) => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try{
+                const response = await axios.get("http://localhost:5001/api/tasks");
+                setTasks(response.data);
+            }catch(error){
+                console.log('Error fetching tasks:', error);
+            }
+        };
+        fetchTasks();
+    }, []);
+
+    return(
+        <>
+            <h2>Lista de Tareas</h2>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task._id}>
+                        <span>{task.titulo}</span>
+                        <button onClick={() => onEdit(task)}>Editar</button>
+                        <button onClick={() => onDelete(task._id)}>Eliminar</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+
+    )
+}
+
+export default TaskList;
